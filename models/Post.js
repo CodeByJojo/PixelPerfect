77ff8d0b3cbcf5b43b18799aaa94278392c8740e
Joe Maltba <joe.maltba@gmail.com>
2022-10-29 05:04:06 -0400

    Added Schemas

---
+++ models/Post.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        require: true,
+    },
+    couldinaryId: {
+        type: String,
+        require: true,
+    },
+    caption: {
+        type: String,
+        required: true,
+    },
+    likes: {
+        type: Number,
+        required: true,
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    slotNum: {
+        type: Number,
+        required: true,
+    }
+});
+
+module.exports = mongoose.model('Post', PostSchema);
